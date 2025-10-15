3838b8ac3af03e2364af6cdc7fcba0aa22bd3766
BrayanRojasLemus <brayanrojas@wposs.com>
2025-10-14 20:29:30 -0500

    Añadir soporte para Swagger y CRUD de Clientes
    Se ha añadido la referencia al paquete `Swashbuckle.AspNetCore` para la generación de documentación Swagger.
    Se ha implementado el controlador `ClienteController` con métodos para listar, agregar y eliminar clientes.
    Además, se ha modificado `DbBancoPruebaContext` para leer la cadena de conexión desde la configuración de la aplicación.
    Se han añadido configuraciones en `Program.cs` para habilitar Swagger y se ha actualizado `appsettings.json` con la cadena de conexión a la base de datos SQL Server.

---
+++ BancoApi/Controllers/ClienteController.cs
@@ -0,0 +1,50 @@
+﻿using BancoApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly DbBancoPruebaContext _dbBankContext;
+        public ClienteController(DbBancoPruebaContext dbBankContext)
+        {
+            _dbBankContext = dbBankContext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+
+        public async Task<IActionResult> ListClients()
+        {
+            var listClient = await _dbBankContext.Clientes.ToListAsync();
+            return Ok(listClient);
+        }
+
+        [HttpPost]
+        [Route("Agregar")]
+
+        public async Task<IActionResult> Agregar([FromBody] Cliente requets)
+        {
+            await _dbBankContext.Clientes.AddAsync(requets);
+            await _dbBankContext.SaveChangesAsync();
+            return Ok(requets);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        { 
+            var clientDelete = await _dbBankContext.Clientes.FindAsync(id);
+            if (clientDelete == null)            
+                return NotFound("No existe el Cliente");
+                _dbBankContext.Clientes.Remove(clientDelete);
+                await _dbBankContext.SaveChangesAsync();
+                return Ok();             
+        }
+        
+    }
+}
